e0b89c2f1483945525b2eaf94046cd7b169a536d
kjbye5252 <37642604+kjbye5252@users.noreply.github.com>
2018-05-30 09:24:26 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,36 +1,36 @@
 
 function setup() {
   createCanvas(200,200);
-//   translate(width/2,height/2);
-//   angleMode(DEGREES);
-//   stroke(255);
+  translate(width/2,height/2);
+  angleMode(DEGREES);
+  stroke(255);
 }
 
 function draw() {
   background(51);
-//   ellipse(0,0,250);
-//   secondshand();
-//   minuteshand();
-//   hourhand();
+  ellipse(0,0,250);
+  secondshand();
+  minuteshand();
+  hourhand();
 }
 
-// function secondshand(){
-//   push();
-//   rotate(second()*6);
-//   line(0,0,0,100);
-//   pop();
-// }  
+function secondshand(){
+  push();
+  rotate(second()*6);
+  line(0,0,0,100);
+  pop();
+}  
 
-// function minuteshand(){
-//   push();
-//   rotate(minute()*6);
-//   line(0,0,0,100);
-//   pop();
-// }  
+function minuteshand(){
+  push();
+  rotate(minute()*6);
+  line(0,0,0,100);
+  pop();
+}  
 
-// function hourhand(){
-//   push();
-//   rotate(hour()*30);
-//   line(0,0,0,100);
-//   pop();
-// }  
+function hourhand(){
+  push();
+  rotate(hour()*30);
+  line(0,0,0,100);
+  pop();
+}  
